200f99ce90a35d50c584cfa2a1b7b56b7f485b14
Kukushkin Alexander <alex@alexkuk.ru>
2020-03-25 17:32:23 +0300

    Fix log

--- slovnet/log.py
+++ slovnet/log.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 def log(format, *args):
     message = format % args
-    now = datetime.now().stftime('%Y-%m-%d %H:%M:%S')
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print(
         '[%s] %s' % (now, message),
         file=sys.stderr
